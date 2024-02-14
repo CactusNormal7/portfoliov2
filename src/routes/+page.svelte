66d312110db117be105a5c13a54400ceb62f9cce
Bjules <jules.besson@ynov.com>
2024-02-14 14:48:22 +0100

    d

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,120 +1,396 @@
 <script>
-	// @ts-ignore
-	// let tittleunderline = [];
+	// @ts-nocheck
+	import { onMount } from 'svelte';
 
-	// const enterTittleColor = () => {
-	// 	for (let i = 0; i < tittleunderline.length; i++) {
-	// 		// @ts-ignore
-	// 		if (i === 0) {
-	// 			// @ts-ignore
-	// 			tittleunderline[i].style.width = '40%';
-	// 		} else {
-	// 			// @ts-ignore
-	// 			tittleunderline[i].style.width = '100%';
-	// 		}
-	// 	}
-	// };
-	// const leaveTittleColor = () => {
-	// 	for (let i = 0; i < tittleunderline.length; i++) {
-	// 		// @ts-ignore
-	// 		tittleunderline[i].style.width = '0%';
-	// 	}
-	// };
+	const projects = [
+		{
+			id: 1,
+			name: 'SafeArea',
+			url: 'safearea.fr',
+			image: 'ratio',
+			tittle: 'Safe Area',
+			description: 'blabla'
+		},
+		{
+			id: 2,
+			name: 'MonsterBlitz',
+			url: 'safearea.fr',
+			image: 'ratio',
+			tittle: 'Monster Blitz',
+			description: 'blabla'
+		}
+	];
+
+	let currentSection = 0;
+	const sections = [];
+	let cursor = null;
+	let insideCursor = null;
+
+	//mouse speed
+	let lastMouseX = 0;
+	let lastMouseY = 0;
+	let speedX = 0;
+	let speedY = 0;
+	let marge;
+	let cas = 0;
+
+	let scrollLevel = 0;
+
+	//____________________________________
+
+	let elementToAnimate;
+	let isinter = false;
+
+	const observeElement = () => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					isinter = true;
+					console.log(isinter)
+				} else {
+					isinter = false;
+					console.log(isinter)
+				}
+			});
+		});
+
+		observer.observe(elementToAnimate);
+	};
+	//____________________________________
+
+	const handleScroll = (e) => {
+		if (e.deltaY > 0 && currentSection < sections.length - 1) {
+			currentSection++;
+		} else if (e.deltaY < 0 && currentSection > 0) {
+			currentSection--;
+		}
+		scrollToSection(currentSection);
+	};
+
+	const handleMouseMove = (e) => {
+		const currentMouseX = e.clientX;
+		const currentMouseY = e.clientY;
+		scrollLevel = window.scrollY;
+		if (lastMouseX !== undefined && lastMouseY !== undefined) {
+			const deltaX = Math.abs(currentMouseX - lastMouseX);
+			const deltaY = Math.abs(currentMouseY - lastMouseY);
+			const speed = Math.sqrt(deltaX * deltaX + deltaY * deltaY); // Magnitude de la vitesse
+
+			// Ajustez le diamètre du curseur en fonction de la vitesse
+			let cursorDiameter = Math.max(40 + speed * 3, 20); // Taille minimale de 20px
+			if (cursorDiameter > 200) {
+				cursorDiameter = 200;
+			}
+			marge = Math.floor(cursorDiameter / 3);
+			cursor.style.width = `${cursorDiameter}px`;
+			cursor.style.height = `${cursorDiameter}px`;
+		}
+
+		cursor.style.left = `${currentMouseX - marge}px`;
+		cursor.style.top = `${currentMouseY - marge + scrollLevel}px`;
+
+		insideCursor.style.left = `${currentMouseX - 5}px`;
+		insideCursor.style.top = `${currentMouseY - 5 + scrollLevel}px`;
+
+		lastMouseX = currentMouseX;
+		lastMouseY = currentMouseY;
+	};
+
+	function mouseEnterButtonNavBar() {
+		cursor.style.visibility = 'hidden';
+	}
+
+	function mouseLeaveButtonNavBar() {
+		cursor.style.visibility = 'visible';
+	}
+
+	function scrollToSection(index) {
+		const targetPosition = sections[index].offsetTop;
+		window.scrollTo({
+			top: targetPosition,
+			behavior: 'smooth'
+		});
+	}
+	onMount(observeElement);
 </script>
 
-<body>
-	<div id="AllContainer">
-		<div id="tittleSection">
-			<!-- <div id="circle1"></div> -->
-			<div style="padding-top: 25px;">
-				<h1 class="tittleText" id="tittleText" style="width: fit-content;">Hi, i'm Jules,</h1>
-				<h1 class="tittleText">a student in web developpement</h1>
+<body role="application" on:wheel={handleScroll} on:mousemove={handleMouseMove}>
+	<div class="absoluteElements">
+		<div bind:this={cursor} class="circleMouse" />
+		<div bind:this={insideCursor} class="insideCircle" />
+		<div class="navbarDiv">
+			<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+			<button
+				on:mouseleave={mouseLeaveButtonNavBar}
+				on:mouseenter={mouseEnterButtonNavBar}
+				on:click={scrollToSection.bind(this, 0)}
+				class="navbarButton">about me</button
+			>
+			<div class="spacerNavbar" />
+			<button
+				on:mouseleave={mouseLeaveButtonNavBar}
+				on:mouseenter={mouseEnterButtonNavBar}
+				on:click={scrollToSection.bind(this, 1)}
+				class="navbarButton">projects</button
+			>
+			<div class="spacerNavbar" />
+			<button
+				on:mouseleave={mouseLeaveButtonNavBar}
+				on:mouseenter={mouseEnterButtonNavBar}
+				on:click={scrollToSection.bind(this, 2)}
+				class="navbarButton">test</button
+			>
+			<div class="spacerNavbar" />
+			<button
+				on:mouseleave={mouseLeaveButtonNavBar}
+				on:mouseenter={mouseEnterButtonNavBar}
+				on:click={scrollToSection.bind(this, 3)}
+				class="navbarButton">test</button
+			>
+		</div>
+	</div>
+
+	<div class="section" id="sec1" bind:this={sections[0]}>
+		<div class="MainTittleSection">
+			<h1 class="TitleSection" id="mainTittle">WELCOME</h1>
+		</div>
+	</div>
+
+	<div class="section" id="sec2" bind:this={sections[1]}>
+		<div id="UnderSection1sec2">
+			<div class="MainTittleSection">
+				<div bind:this={elementToAnimate} ></div>
+				{#if isinter}
+					<h1 id="projectTittle" class="animate-test TitleSection">Project</h1>
+				{:else}
+					<h1 id="projectTittle" class="TitleSection">Project</h1>
+				{/if}
 			</div>
+			<div id="cardSection">
+				{#each projects as project}
+					<li key={project.id}>{project.tittle}</li>
+				{/each}
+			</div>
+		</div>
+	</div>
+
+	<div class="section" id="sec3" bind:this={sections[2]}>
+		<div class="MainTittleSection">
+			<h1 id="mainTittle">WELCOME</h1>
 		</div>
 	</div>
 </body>
 
+<!-- 
+----colors----
+
+black : 141416
+grey : 2f2f31
+
+-->
+
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;500&display=swap');
+
+	* {
+		font-family: 'Roboto', sans-serif;
+		padding: 0;
+		overflow: hidden;
+		margin: 0;
+		cursor: none;
+	}
 
 	body {
 		padding: 0;
 		margin: 0;
 	}
 
-	#tittleSection {
-		widows: 100%;
-		position: relative;
+	.circleMouse {
+		position: absolute;
+		width: 100px;
+		height: 100px;
+		border-radius: 50%;
+		transition: 0.05s ease;
+		z-index: 20;
+		background-color: white;
+		mix-blend-mode: difference; /* Changez le mode de fusion selon vos préférences */
+		pointer-events: none; /* Assurez-vous que le cercle ne bloque pas les événements de la souris */
+	}
+
+	.insideCircle {
+		position: absolute;
+		width: 20px;
+		z-index: 20;
+		height: 20px;
+		border-radius: 50%;
+		background-color: white;
+		mix-blend-mode: difference;
+		opacity: 70%;
+		pointer-events: none;
+	}
+
+	.section {
 		display: flex;
+		width: 100vw;
+		height: 100vh;
+		justify-content: center;
 		flex-direction: column;
 		align-items: center;
 	}
 
-	#circle1 {
-		position: absolute;
-		background-color: white;
-		top: -20%;
-		width: 400px;
-		left: 15%;
-		height: 400px;
-		border-radius: 100%;
-		mix-blend-mode: difference;
+	#sec1 {
+		background-color: #141416;
 	}
 
-	#AllContainer {
+	#sec2 {
+		background-color: #141416;
+	}
+
+	#UnderSection1sec2 {
+		width: 70%;
 		display: flex;
 		flex-direction: column;
-		width: 100vw;
-		height: 100vh;
-		background-color: black;
+		height: 60%;
 	}
 
-	.tittleText {
-		margin-bottom: 0;
-		margin-top: 0;
-		border-bottom: 5px solid black;
+	#sec3 {
+		background-color: #141416;
+	}
+
+	.TitleSection {
+		font-size: 200px;
 		color: white;
-		position: relative;
-		font-weight: bolder;
-		padding: 0px 10px 0px 10px;
-		transition: 0.5s;
-		animation: 0.8s ease titteComing;
+		margin: 0;
+		user-select: none;
+		margin-bottom: 100px;
+	}
+
+	#mainTittle {
+		animation: tracking-in-expand 1s ease forwards, letter-shadow 2s ease forwards;
+	}
+
+	#projectTittle {
+		font-size: 150px;
+	}
+
+	.animate-test {
+		animation: tracking-in-expand 0.5s ease forwards, letter-shadow 1s ease forwards;
+	}
+
+	.navbarDiv {
+		position: fixed;
+		top: 90%;
+		left: 50%;
+		display: flex;
+		justify-content: space-between;
+		width: 30%;
+		align-items: center;
+		overflow: visible;
+		transform: translate(-50%, -50%);
+		height: 90px;
+	}
+
+	.navbarButton {
+		min-width: 70px;
+		min-height: 70px;
+		border-radius: 1000px;
+		background-color: #141416;
 		font-family: 'Roboto', sans-serif;
-		font-size: 60px;
+		border: 2px solid #2f2f31;
+		color: #2f2f31;
+		font-size: 80%;
+		z-index: 19;
+		font-weight: bolder;
+		position: relative;
 	}
 
-	.tittleText:after {
+	.navbarButton::before {
 		content: '';
+		display: block;
+		width: 0px;
+		height: 0px;
+		background-color: white;
+		border-radius: 50%;
 		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		transition: width 0.3s, height 0.3s;
+	}
+
+	.navbarButton:hover {
+		color: #141416;
+		transform: scale(1.1); /* Agrandit le bouton au survol */
+	}
+
+	.navbarButton:hover::before {
+		width: 90px;
+		height: 90px;
+		z-index: -1;
+	}
+
+	.spacerNavbar {
 		width: 100%;
-		height: 8px;
-		background-color: white; /* Couleur du soulignement */
-		bottom: 0;
-		left: 0;
-		transform: translateY(10px);
-		transform-origin: right;
-		transition: transform 0.3s ease;
-		animation: 1s ease tittleunderline;
+		height: 2px;
+		background-color: #2f2f31;
 	}
 
-	@keyframes titteComing {
+	.navbarA:hover {
+		color: white;
+		transition: 1s ease;
+	}
+
+	.MainTittleSection {
+		display: flex;
+	}
+
+	#cardSection {
+		background-color: red;
+	}
+
+	/* @-webkit-keyframes tracking-in-expand {
 		0% {
-			transform: translateY(-200px);
+			letter-spacing: -0.5em;
+			opacity: 0;
 		}
-
+		40% {
+			opacity: 0.6;
+		}
 		100% {
-			transform: translateY(0px);
+			opacity: 1;
 		}
-	}
-
-	@keyframes tittleunderline {
+	} */
+	@keyframes tracking-in-expand {
 		0% {
-			width: 0;
+			letter-spacing: -0.5em;
+			opacity: 0;
 		}
-
+		40% {
+			opacity: 0.6;
+		}
 		100% {
-			width: 100;
+			opacity: 1;
 		}
 	}
 
+	@keyframes letter-shadow {
+		0% {
+			text-shadow: 0px 0px #2f2f31;
+		}
+		40% {
+			text-shadow: 5px 5px #2f2f31;
+		}
+		100% {
+			text-shadow: 15px 15px #2f2f31;
+		}
+	}
+
+	@keyframes project-appear {
+		0% {
+			color: red;
+		}
+		100% {
+			color: blue;
+		}
+	}
 </style>
